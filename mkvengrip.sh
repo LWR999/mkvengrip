2c9c5757dc39d1c4fa90f89b4cef42da97937440
Leon Rees <leon.x.rees@gmail.com>
2024-01-04 09:27:45 +0000

    Initial Commit

---
+++ mkvengrip.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+# mkvengrip - return english audio and subtitle track IDs for an mkvfile
+input_file="$1"
+
+echo "STATUS: Processing input file $1"
+
+mkvinfo_output=$(mkvinfo "$input_file")
+
+line_count=0
+section_id=""
+section_type=""
+section_count=0
+eng_audio_tracks=0
+audio_tracks=""
+subtitle_tracks=""
+
+while IFS= read -r line; do
+  if [[ $line == "(MKVInfo) Error:"* ]]; then
+    echo "ERROR: cannot open file $1"
+    exit 1
+  fi
+  if [[ $line == "| + Track" ]]; then
+    if [[ $section_id != "" && $section_type != "" ]]; then
+      if [[ $section_language == "" ]]; then
+        echo "ID: $((section_id)), Track Type: $section_type"
+        if [[ $section_type == "audio" ]]; then
+          if [[ $audio_tracks == "" ]]; then
+            audio_tracks="--audio-tracks $((section_id))"
+          else
+            audio_tracks="$audio_tracks,$((section_id))"
+          fi
+        elif [[ $section_type == "subtitles" ]]; then
+          if [[ $subtitle_tracks == "" ]]; then
+            subtitle_tracks="--subtitle-tracks $((section_id))"
+          else
+            subtitle_tracks="$subtitle_tracks,$((section_id))"
+          fi
+        fi
+      else 
+        if [[ $section_type == "audio" ]]; then
+          section_count=$((section_count + 1))
+        fi
+      fi
+    fi   
+    section_id=""
+    section_type=""
+    section_language=""
+  elif [[ $line =~ "|  + Track number: "* ]]; then
+    section_id=$(echo "$line" | sed 's/|  + Track number: \(.*\) (.*/\1/')    
+    section_id=$((section_id - 1))
+  elif [[ $line == "|  + Track type: "* ]]; then
+    section_type=${line#"|  + Track type: "}
+  elif [[ $line == "|  + Language: "* ]]; then
+    if [[ $line != "|  + Language: eng"* && $line != "|  + Language: und"* ]]; then
+      section_language=${line#"|  + Language: "}
+    fi
+    if [[ $section_type == "audio" ]]; then
+      eng_audio_tracks=$((eng_audio_tracks + 1))
+    fi
+  fi
+
+done <<< "$mkvinfo_output"
+
+if [[ $section_id != "" && $section_type != "" ]]; then
+  if [[ $section_language == "" ]]; then
+    echo "ID: $((section_id)), Track Type: $section_type"
+    if [[ $section_type == "audio" ]]; then
+      if [[ $audio_tracks == "" ]]; then
+        audio_tracks="--audio-tracks $((section_id))"
+      else
+        audio_tracks="$audio_tracks,$((section_id))"
+      fi
+    elif [[ $section_type == "subtitles" ]]; then
+      if [[ $subtitle_tracks == "" ]]; then
+        subtitle_tracks="--subtitle-tracks $((section_id))"
+      else
+        subtitle_tracks="$subtitle_tracks,$((section_id))"
+      fi
+    fi
+  else 
+    if [[ $section_type == "audio" ]]; then
+      section_count=$((section_count + 1))
+    fi
+  fi
+fi
+
+echo "Number foreign language audio tracks: $section_count"
+echo "Number english language audio tracks: $eng_audio_tracks"
+if [[ $section_count > 0 && $eng_audio_tracks > 0 ]]; then
+  if [[ $audio_tracks != "" ]]; then
+    echo "$audio_tracks"
+  fi
+  if [[ $subtitle_tracks != "" ]]; then
+    echo "$subtitle_tracks"
+  fi
+else
+  echo "No work for MKVMERGE to do : file has ENG or Foreign audio only"
+fi
+echo
---
+++ mkvprepcmd.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+# mkvprepcmd - return an mkvmerge command for stripping english audio and subtitle tracks from an mkvfile
+input_file="$1"
+escaped_input_file=$(printf "%q" "$1")
+
+mkvinfo_output=$(mkvinfo "$input_file")
+
+line_count=0
+section_id=""
+section_type=""
+section_count=0
+audio_tracks=""
+subtitle_tracks=""
+
+while IFS= read -r line; do
+  if [[ $line == "(MKVInfo) Error:"* ]]; then
+    echo "ERROR: cannot open file $1"
+    exit 1
+  fi
+  if [[ $line == "| + Track" ]]; then
+    if [[ $section_id != "" && $section_type != "" ]]; then
+      if [[ $section_language == "" ]]; then
+        if [[ $section_type == "audio" ]]; then
+          if [[ $audio_tracks == "" ]]; then
+            audio_tracks="--audio-tracks $((section_id))"
+          else
+            audio_tracks="$audio_tracks,$((section_id))"
+          fi
+        elif [[ $section_type == "subtitles" ]]; then
+          if [[ $subtitle_tracks == "" ]]; then
+            subtitle_tracks="--subtitle-tracks $((section_id))"
+          else
+            subtitle_tracks="$subtitle_tracks,$((section_id))"
+          fi
+        fi
+      else 
+        if [[ $section_type == "audio" ]]; then
+          section_count=$((section_count + 1))
+        fi
+      fi
+    fi   
+    section_id=""
+    section_type=""
+    section_language=""
+  elif [[ $line =~ "|  + Track number: "* ]]; then
+    section_id=$(echo "$line" | sed 's/|  + Track number: \(.*\) (.*/\1/')    
+    section_id=$((section_id - 1))
+  elif [[ $line == "|  + Track type: "* ]]; then
+    section_type=${line#"|  + Track type: "}
+  elif [[ $line == "|  + Language: "* ]]; then
+    if [[ $line != "|  + Language: eng"* && $line != "|  + Language: und"* ]]; then
+      section_language=${line#"|  + Language: "}
+    fi
+    if [[ $section_type == "audio" ]]; then
+      eng_audio_tracks=$((eng_audio_tracks + 1))
+    fi
+  fi
+done <<< "$mkvinfo_output"
+
+if [[ $section_id != "" && $section_type != "" ]]; then
+  if [[ $section_language == "" ]]; then
+    if [[ $section_type == "audio" ]]; then
+      if [[ $audio_tracks == "" ]]; then
+        audio_tracks="--audio-tracks $((section_id))"
+      else
+        audio_tracks="$audio_tracks,$((section_id))"
+      fi
+    elif [[ $section_type == "subtitles" ]]; then
+      if [[ $subtitle_tracks == "" ]]; then
+        subtitle_tracks="--subtitle-tracks $((section_id))"
+      else
+        subtitle_tracks="$subtitle_tracks,$((section_id))"
+      fi
+    fi
+  else 
+    if [[ $section_type == "audio" ]]; then
+      section_count=$((section_count + 1))
+    fi
+  fi
+fi
+
+if [[ $section_count > 0 && $eng_audio_tracks >0 ]]; then
+  echo "# START : $1"
+  echo "mv $escaped_input_file input.mkv"
+  echo "mkvmerge -o $escaped_input_file $audio_tracks $subtitle_tracks input.mkv"
+  if [ $? -eq 0 ]; then
+    echo "rm input.mkv"
+    echo "# SUCCESS : $1"
+  else
+    echo "mv input.mkv $escaped_input_file"
+    echo "# FAILURE : $1"
+  fi
+  echo ""
+fi
